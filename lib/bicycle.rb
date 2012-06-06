8f7a5bd8017422212a805dc14d3bbd8d4374d564
Stuart Chinery <stuart.chinery@headlondon.com>
2012-06-06 09:49:23 +0100

    Updated comment

--- lib/bicycle.rb
+++ lib/bicycle.rb
@@ -1,8 +1,8 @@
 module Bicycle
 
-  # Function to return a single value from an array of 
+  # Function to return a single value from an array of
   # given values that you want to cycle through.
-  # @param [Array] values to cycle through
+  # @param [Array] *to_cycle - values to cycle through
   # @return [?] one of the values in a given order
   def self.cycle(*to_cycle)
     return to_cycle[@_cycle = ((@_cycle || -1) + 1) % to_cycle.count]
